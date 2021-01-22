2e7159e205c8ca0f1eb9d08dccd3cc5a06006ead
Flodaden <44039692+Flodaden@users.noreply.github.com>
2021-01-22 21:56:05 +0100

    Add All Files

---
+++ Invoicing/Server/Models/BusinessDataSql.cs
@@ -0,0 +1,42 @@
+﻿using Dapper;
+using Invoicing.Shared;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Invoicing.Server.Models
+{
+    public class BusinessDataSql : IBusinessData
+    {
+        private SqlConnection cnct;
+
+        public BusinessDataSql(string connectionString)
+        {
+            cnct = new SqlConnection(connectionString);
+        }
+
+        public void Dispose()
+        {
+            cnct.Dispose();
+        }
+        public IEnumerable<Invoice> AllInvoices => throw new NotImplementedException();
+
+        public decimal SalesRevenue => throw new NotImplementedException();
+
+        public decimal Outstanding => throw new NotImplementedException();
+
+        public void Add(Invoice newFacture)
+        {
+            var insert = @"INSERT INTO Factures(Rerence,Customer,Amount,Created,Paid,Expected) values (@Reference,@Customer,@Amount,@Created,@Paid,@Expected)";
+
+            newFacture.Expected = newFacture.Created.AddMonths(1);
+
+            cnct.Query(insert, newFacture);
+        }
+
+    }
+
+
+}
---
+++ Invoicing/Shared/BusinessTestData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Invoicing.Shared
+{
+    public class BusinessTestData : IBusinessData
+    {
+        private Invoice[] testInvoices =
+        {
+            new Invoice("BSO349", "FOO", 2123.4m     , DateTime.Now),
+            new Invoice("BSO345", "BAR", 5000.45m    , DateTime.Now),
+            new Invoice("BSO450", "BAR", 123.4m      , DateTime.Now),
+            new Invoice("BSO410", "BOO", 1000.12m   , DateTime.Now)
+        };
+
+        public BusinessTestData()
+        {
+            testInvoices[1].RegisterPayment(DateTime.Now, 5000.45m);
+            testInvoices[3].RegisterPayment(DateTime.Now, 500.46m);
+            testInvoices[3].Expected = DateTime.Now;
+        }
+        
+        public IEnumerable<Invoice> AllInvoices => testInvoices;
+
+        public decimal SalesRevenue => testInvoices.Sum(invoice => invoice.Amount);
+
+        public decimal Outstanding => testInvoices.Sum(invoice => invoice.Amount - invoice.Paid);
+    }
+}
